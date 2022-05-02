16f6b4e2e97464fc6fa1245bfc01124bd3245b19
stfibonacci <87790340+stfibonacci@users.noreply.github.com>
2022-05-02 16:41:52 -0700

    first commit

---
+++ contracts/LeverageGearboxYearn.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from "../interfaces/tokens/ERC20.sol";
+import {ERC4626} from "../interfaces/mixins/ERC4626.sol";
+
+import {IYVault} from "../interfaces/gearbox/IYVault.sol";
+import {ICreditAccount} from "../interfaces/gearbox/ICreditAccount.sol";
+import {ICreditFilter} from "../interfaces/gearbox/ICreditFilter.sol";
+import {ICreditManager} from "../interfaces/gearbox/ICreditManager.sol";
+
+contract LeverageGearboxYearn is ERC4626, Ownable {
+    address public creditAccount;
+    ICreditManager public creditManager;
+    ICreditFilter public creditFilter;
+    IYVault public yearnAdapter;
+    uint256 public leverage;
+
+    constructor(
+        ERC20 _asset,
+        address _yearnAdapter,
+        address _creditManager,
+        uint256 _leverage
+    ) ERC4626(_asset, " Leverage Gearbox Yearn DAI", "lgyDAI") {
+        require(_yearnAdapter != address(0));
+        require(_creditManager != address(0));
+        require(
+            _leverage >= 100 && _leverage <= 300,
+            "leverage must be between 100 and 300"
+        );
+
+        yearnAdapter = IYVault(_yearnAdapter);
+        creditManager = ICreditManager(_creditManager);
+        creditFilter = ICreditFilter(creditManager.creditFilter());
+        leverage = _leverage;
+    }
+
+    modifier onlyAuthorized() {
+        require(msg.sender == owner(), "only authorized");
+        _;
+    }
+
+    function openAccount(uint256 assets) external onlyAuthorized {
+        require(assets != 0, "ZERO_ASSETS");
+        asset.approve(address(creditManager), type(uint256).max);
+        creditManager.openCreditAccount(assets, address(this), leverage, 0);
+        creditAccount = creditManager.getCreditAccountOrRevert(address(this));
+    }
+
+    function investToYearn() external onlyAuthorized {
+        uint256 _assets = asset.balanceOf(creditAccount);
+        require(_assets != 0, "ZERO_ASSETS");
+        yearnAdapter.deposit(_assets);
+    }
+
+    function withdrawFromYearn() public onlyAuthorized {
+        uint256 yShares = yearnAdapter.balanceOf(creditAccount);
+        require(yShares != 0, "ZERO_ySHARES");
+        yearnAdapter.withdraw(yShares);
+    }
+
+    function addCollateral(uint256 assets) external onlyAuthorized {
+        require(assets != 0, "ZERO_ASSETS");
+        creditManager.addCollateral(address(this), address(asset), assets);
+    }
+
+    function closeAccount() external onlyAuthorized {
+        uint256 yShares = yearnAdapter.balanceOf(creditAccount);
+        if (yShares != 0) yearnAdapter.withdraw(yShares);
+
+        creditManager.repayCreditAccount(address(this));
+    }
+
+    function getCreditAccount() public view returns (address) {
+        return creditManager.creditAccounts(address(this));
+    }
+
+    function getHealthFactor() public view returns (uint256) {
+        return
+            creditFilter.calcCreditAccountHealthFactor(address(creditAccount));
+    }
+
+    function getBorrowedAmount() public view returns (uint256) {
+        return ICreditAccount(creditAccount).borrowedAmount();
+    }
+
+    function getTotalValue() public view returns (uint256) {
+        return creditFilter.calcTotalValue(address(creditAccount));
+    }
+
+    function getCollateralValue() public view returns (uint256) {
+        if (creditManager.hasOpenedCreditAccount(address(this))) {
+            uint256 totalValue = getTotalValue();
+            uint256 totalBorrwed = getBorrowedAmount();
+            return totalValue - totalBorrwed;
+        } else {
+            return 0;
+        }
+    }
+
+    function availableAssets() public view returns (uint256) {
+        return asset.balanceOf(address(this));
+    }
+
+    function totalAssets() public view override returns (uint256) {
+        return availableAssets() + getCollateralValue();
+    }
+}
---
+++ scripts/invest_yearn.py
@@ -0,0 +1,67 @@
+from brownie import LeverageGearboxYearn, accounts
+
+from brownie_tokens import MintableForkToken
+
+
+def deploy_leverage_gerarbox_yearn():
+    dai_address = "0x6b175474e89094c44da98b954eedeac495271d0f"
+    yearn_dai_adapter = "0x403E98b110a4DC89da963394dC8518b5f0E2D5fB"
+    creditManager = "0x777e23a2acb2fcbb35f6ccf98272d03c722ba6eb"
+    leverage = "300"
+    leverage_gearbox_yearn = LeverageGearboxYearn.deploy(
+        dai_address,
+        yearn_dai_adapter,
+        creditManager,
+        leverage,
+        {"from": accounts[0]},
+    )
+
+    decimal = 10 ** 18
+    a_amount = 20_000 * decimal
+    b_amount = 10_000 * decimal
+
+    alice = accounts[0]
+    bob = accounts[1]
+    dai_address = "0x6b175474e89094c44da98b954eedeac495271d0f"
+    dai = MintableForkToken(dai_address)
+    dai._mint_for_testing(alice, a_amount)
+
+    dai.approve(leverage_gearbox_yearn, a_amount, {"from": alice})
+    leverage_gearbox_yearn.deposit(a_amount, alice, {"from": alice})
+
+    collateral_amount = leverage_gearbox_yearn.availableAssets() / 2
+    print("Opening credit account ...")
+    leverage_gearbox_yearn.openAccount(collateral_amount)
+    print("Depositing into Yearn ...")
+    leverage_gearbox_yearn.investToYearn()
+
+    print("Credit account opened and invested into Yearn")
+    print(f"CA health factor: {leverage_gearbox_yearn.getHealthFactor() / 10 ** 4}")
+    print(f"CA borrowed amount:{leverage_gearbox_yearn.getBorrowedAmount() / decimal}")
+    print(f"CA total amount:{leverage_gearbox_yearn.getTotalValue() / decimal}")
+    print(f"CA collateral :{leverage_gearbox_yearn.getCollateralValue()/ decimal}")
+
+    print("Adding collateral ...")
+    leverage_gearbox_yearn.addCollateral(leverage_gearbox_yearn.availableAssets())
+    print(
+        f"Current health factor: {leverage_gearbox_yearn.getHealthFactor() / 10 ** 4}"
+    )
+    print(f"Current collateral :{leverage_gearbox_yearn.getCollateralValue()/ decimal}")
+
+    print("Withdrawing from Yearn ...")
+    leverage_gearbox_yearn.withdrawFromYearn()
+    print("Closing Credit Account ...")
+    leverage_gearbox_yearn.closeAccount()
+
+    print(f"CA address: {leverage_gearbox_yearn.getCreditAccount()}")
+    print(
+        f"contract total dai balance: {leverage_gearbox_yearn.totalAssets()/ decimal} "
+    )
+
+    alice_shares = leverage_gearbox_yearn.balanceOf(alice)
+    leverage_gearbox_yearn.redeem(alice_shares, alice, alice, {"from": alice})
+    print(f"alice dai balance is {dai.balanceOf(alice)/ decimal}")
+
+
+def main():
+    deploy_leverage_gerarbox_yearn()
